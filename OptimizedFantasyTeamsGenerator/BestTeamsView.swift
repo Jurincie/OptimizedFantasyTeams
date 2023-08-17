21cc67175ff3ef1fce38887458be51d0586bc1c8
Ron Jurincie <ronjurincie@gmail.com>
2023-08-16 21:07:24 -0400

    still need to delete app for each run

---
+++ OptimizedFantasyTeamsGenerator/BestTeamsView.swift
@@ -0,0 +1,18 @@
+//
+//  BestTeamsView.swift
+//  OptimizedFantasyTeamsGenerator
+//
+//  Created by Ron Jurincie on 8/14/23.
+//
+
+import SwiftUI
+
+struct BestTeamsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BestTeamsView()
+}
---
+++ OptimizedFantasyTeamsGenerator/Team.swift
@@ -0,0 +1,8 @@
+//
+//  Team.swift
+//  OptimizedFantasyTeamsGenerator
+//
+//  Created by Ron Jurincie on 8/14/23.
+//
+
+import Foundation
